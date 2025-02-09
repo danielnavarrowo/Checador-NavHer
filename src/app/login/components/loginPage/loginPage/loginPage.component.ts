bcadba98c3c805d50ad542f56573d209966f58d2
Daniel Navarro <dani.navarro281@gmail.com>
2025-02-09 15:26:00 -0600

    Enhance login page with guest login option and validation messages

--- src/app/login/components/loginPage/loginPage/loginPage.component.ts
+++ src/app/login/components/loginPage/loginPage/loginPage.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject, OnInit, signal } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SupabaseService } from '../../../../services/supabase.service';
 import { Router } from '@angular/router';
@@ -14,27 +14,32 @@ import { Router } from '@angular/router';
 })
 export class LoginPageComponent {
 
+
   private fb = inject(FormBuilder);
   private supabase = inject(SupabaseService);
   private router = inject(Router);
 
-  public loginForm: FormGroup = this.fb.group({
-    email: ['', Validators.required],
-    password: ['', Validators.required]
-  });
-  errorMsg: string | null = null;
 
+  public loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(4)]]
+  });
+
+  fillAsGuest() {
+    this.loginForm.setValue({
+      email: 'guest@guest.com',
+      password: 'guest123'
+    });
+  }
+
+  errorMsg = signal<string | null>(null);
+  
   onSubmit(): void {
     if (this.loginForm.valid) {
       this.supabase.login(this.loginForm.value.email, this.loginForm.value.password).subscribe(
         (response) => {
-          if (response.error) {
-            this.errorMsg = response.error.message;
-            console.error('Error logging in:', response.error.message);
-          } else {
-            this.errorMsg = null;
-            this.router.navigate(['/checador']);
-          }
+          if (response.error) this.errorMsg.set(response.error.message);
+          else this.router.navigate(['/productos']);
         }
       );
     }
